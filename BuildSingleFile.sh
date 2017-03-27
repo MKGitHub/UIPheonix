84d9f435b42c71551df6d87399432e183f6228ce
Mohsan Khan <git.mk@xybernic.com>
2017-03-27 22:15:32 +0200

    Update script

--- BuildSingleFile.sh
+++ BuildSingleFile.sh
@@ -3,7 +3,12 @@
 #
 
 
-cat Sources/*.swift > SingleFileSource/UIPheonix_2_0_1.swift
+cat Sources/*.swift > SingleFileSource/UIPheonix.swift
+
+echo "// UIPheonix 2.0.1" > /tmp/UIPheonix_temp
+echo "" >> /tmp/UIPheonix_temp
+cat SingleFileSource/UIPheonix.swift >> /tmp/UIPheonix_temp
+cp /tmp/UIPheonix_temp SingleFileSource/UIPheonix.swift
 
 
 #…
